fefc7619cb9c4235bd5d915ef433dfdf37922f25
ollie <behoya11@gmail.com>
2025-02-11 16:06:22 +0800

    chore: 調整 base 路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/2025-hex-react-training-week02/",
+  base: "/2025-hex-react-training-week03/",
   plugins: [react()],
 });
