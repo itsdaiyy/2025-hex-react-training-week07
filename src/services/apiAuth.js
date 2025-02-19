b5c7385b5cd3ca6ff5b46283e7ebb6d255ad06fa
ollie <behoya11@gmail.com>
2025-02-19 18:02:24 +0800

    style: 移除不必要程式碼

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -13,7 +13,6 @@ export async function login(account) {
 
   try {
     const res = await axios.post(url, account);
-    console.log(res);
     const { expired, token, message } = res.data;
 
     document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
@@ -50,7 +49,7 @@ export async function checkLogin() {
 
     return { success, uid };
   } catch (error) {
-    console.log("未登入", error);
+    console.error("未登入", error);
     return null;
   }
 }
