374ef9076299a52c6af3289c3d4fb326e73c3125
ollie <behoya11@gmail.com>
2025-02-22 22:21:27 +0800

    chore: 調整為 Week05 作業名稱

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/2025-hex-react-training-week04/",
+  base: "/2025-hex-react-training-week05/",
   plugins: [react()],
 });
