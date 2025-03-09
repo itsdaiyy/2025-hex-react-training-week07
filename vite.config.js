5a54ffe9e71ff6ad2217e4354573451a90c7e8b6
ollie <behoya11@gmail.com>
2025-03-09 19:22:56 +0800

    chore: 調整 vite 設定檔 & 專案名稱

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/2025-hex-react-training-week05/",
+  base: "/2025-hex-react-training-week06/",
   plugins: [react()],
 });
